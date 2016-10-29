fcb94c44cbcdb5b9e7f5fac0ffae89e48af2fe08
svfbamilo <saeed.farahani@gmail.com>
2016-10-29 18:09:13 +0330

    auto routing generator

--- src/AppBundle/Command/AppGenerateRoutesCommand.php
+++ src/AppBundle/Command/AppGenerateRoutesCommand.php
@@ -22,19 +22,32 @@ class AppGenerateRoutesCommand extends ContainerAwareCommand
     {
         $output->writeln('start generating routes ...');
 
-        $kernel = $this->createKernel();
-        $kernel->boot();
+        $kernel = $this->getContainer()->get('kernel');
+
+        $root =  dirname($kernel->getRootDir());
         $bundles = $kernel->getBundles();
 
-        foreach ($bundles as $bundle){
-            $output->writeln($bundle);
+        $config = "{$root}/app/config/routing_custom.yml";
+        file_put_contents($config, '');
+        foreach ($bundles as $name => $bundle){
+            if($name != 'AppBundle') {
+                $path = $bundle->getPath();
+                $path = explode('/', ltrim(substr($path, strlen($root)), '/'));
+                if ($path[0] == 'src') {
+                    $output->writeln("- {$name} [".implode('/', $path)."]");
+                    $path = explode('/', strtolower(preg_replace('/[A-Z]/', '/$0', $name)));
+                    array_pop($path);
+                    $route_name = implode('_', $path);
+                    $prefix_prefix = implode('/', $path);
+                    file_put_contents(
+                        $config,
+                        "{$route_name}:\n\tresource:\t\"@{$name}/Controller/\"\n\tprefix:\t\t{$prefix_prefix}\n\n",
+                        FILE_APPEND);
+                }
+            }
         }
 
-
-
-        $output->writeln('ok');
-        $output->writeln('ok');
-        $output->writeln('ok');
+        $output->writeln('finished.');
     }
 
 }
