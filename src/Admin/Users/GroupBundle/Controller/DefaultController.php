87197f9244b6c9a06976a5d43c41d3e19de14db6
saeed <psproot@gmail.com>
2016-10-31 00:25:37 +0330

    routing generator

--- src/Admin/Users/GroupBundle/Controller/DefaultController.php
+++ src/Admin/Users/GroupBundle/Controller/DefaultController.php
@@ -2,10 +2,11 @@
 
 namespace Admin\Users\GroupBundle\Controller;
 
+use AppBundle\Wrappers\AdminPanelController;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
-class DefaultController extends Controller
+class DefaultController extends AdminPanelController
 {
     /**
      * @Route("/", name="admin_users_group")
